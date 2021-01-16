d672daddf0e6db685ff90e49dde46a4cd511d72c
magacula <makoigacula@gmail.com>
2021-01-16 00:32:39 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch("/time")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>The current time is {currentTime}</p>
+    </div>
+  );
+}
+
+export default App;
