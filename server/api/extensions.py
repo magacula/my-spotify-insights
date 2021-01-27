01ce9329aad10c451e81b724dece84d80c052404
lazytosay <siweisijiao.weebly@gmail.com>
2021-01-26 19:18:59 -0800

    Revert "added a rate limiter to limite the rate of access from the same request address"
    This reverts commit d9e9c9b34c8c441ac4227b341dcdd040e7ed058e.

--- server/api/extensions.py
+++ server/api/extensions.py
@@ -1,7 +1,5 @@
 import mysql.connector
 import os
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 #extensions for the backend part
 
@@ -18,7 +16,3 @@ db_cursor = db.cursor()
 db_cursor.execute('USE myspotifyinsight')
 db.commit()
 """
-
-
-#limiter used to limiter the visit rate according to remote address
-limiter = Limiter(key_func=get_remote_address, default_limits=["5 per second"])
