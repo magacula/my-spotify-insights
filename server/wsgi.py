bce04183a2def7e1330bef19365773a919d07601
lazytosay <siweisijiao.weebly@gmail.com>
2021-01-30 12:30:27 -0800

    files for deployment

---
+++ server/wsgi.py
@@ -0,0 +1,11 @@
+#needed for deployment
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from api import create_app
+app = create_app()
+#app = create_app("production")
