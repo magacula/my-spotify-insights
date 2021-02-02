09b0a77a81381bb020a1134768e9d015af97ce27
magacula <makoigacula@gmail.com>
2021-02-01 21:02:35 -0800

    updated return statement for @user_bp.route("/user/recently_played_tracks")

--- server/api/blueprints/user.py
+++ server/api/blueprints/user.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, session, render_template
+from flask import Blueprint, session, render_template, jsonify, make_response
 from server.api.decorators import login_required, token_checked
 from server.api.extensions import limiter
 from server.api.utils import get_spotify_object
+
+import sys
 
 # routes for specific user (information)
 
@@ -14,14 +16,13 @@ user_bp = Blueprint('user', __name__)
 @token_checked
 def home():
     sp = get_spotify_object()
-    #get current user
+    # get current user
     current_user = sp.current_user()
 
-    #some basic user information
+    # some basic user information
 
-    #return "this is user page..."
-    return render_template("user/dashboard_interface.html", user_info = {})
-
+    # return "this is user page..."
+    return render_template("user/dashboard_interface.html", user_info={})
 
 
 @user_bp.route("/user/top_tracks")
@@ -33,24 +34,24 @@ def top_tracks():
 
     top_tracks = []
 
-    #keep get top tracks until there is no more left
+    # keep get top tracks until there is no more left
     offset_count = 0
     limit_count = 50
     while(True):
-        top_tracks_raw = sp.current_user_top_tracks(limit=limit_count, offset=offset_count)
+        top_tracks_raw = sp.current_user_top_tracks(
+            limit=limit_count, offset=offset_count)
         offset_count += limit_count
 
         for one_track in top_tracks_raw['items']:
             one_track_name = one_track['name']
             top_tracks.append(one_track_name)
 
-        #if there are less tracks then limit_count, then no need to do another search
+        # if there are less tracks then limit_count, then no need to do another search
         if len(top_tracks_raw['items']) < limit_count:
             break
 
-    #FIXME: change this part when need to work with front end
+    # FIXME: change this part when need to work with front end
     return render_template("user/top_tracks.html", top_tracks=top_tracks)
-
 
 
 @user_bp.route("/user/top_artists")
@@ -62,26 +63,24 @@ def top_artists():
 
     top_artists = []
 
-    #keep get top artists until there is no more left
+    # keep get top artists until there is no more left
     offset_count = 0
     limit_count = 50
     while(True):
-        top_artists_raw = sp.current_user_top_artists(limit=limit_count, offset=offset_count)
+        top_artists_raw = sp.current_user_top_artists(
+            limit=limit_count, offset=offset_count)
         offset_count += limit_count
 
         for one_artist in top_artists_raw['items']:
             one_artist_name = one_artist['name']
             top_artists.append(one_artist_name)
 
-        #if there are less tracks then limit_count, then no need to do another search
+        # if there are less tracks then limit_count, then no need to do another search
         if len(top_artists_raw['items']) < limit_count:
             break
 
-
-    #FIXME: change this part when need to work with front end
+    # FIXME: change this part when need to work with front end
     return render_template("user/top_artists.html", top_artists=top_artists)
-
-
 
 
 @user_bp.route("/user/top_albums")
@@ -93,11 +92,12 @@ def top_albums():
 
     top_albums = []
 
-    #keep get top tracks until there is no more left
+    # keep get top tracks until there is no more left
     offset_count = 0
     limit_count = 50
     while(True):
-        top_tracks_raw = sp.current_user_top_tracks(limit=limit_count, offset=offset_count)
+        top_tracks_raw = sp.current_user_top_tracks(
+            limit=limit_count, offset=offset_count)
         offset_count += limit_count
 
         for one_track in top_tracks_raw['items']:
@@ -105,15 +105,12 @@ def top_albums():
             if cur_album_name not in top_albums:
                 top_albums.append(cur_album_name)
 
-        #if there are less tracks then limit_count, then no need to do another search
+        # if there are less tracks then limit_count, then no need to do another search
         if len(top_tracks_raw['items']) < limit_count:
             break
 
-
-    #FIXME: change this part when need to work with front end
+    # FIXME: change this part when need to work with front end
     return render_template("user/top_albums.html", top_albums=top_albums)
-
-
 
 
 @user_bp.route("/user/recently_played_tracks")
@@ -132,6 +129,13 @@ def recently_played_tracks():
         one_track = one_record['track']
         recently_played_tracks.append(one_track['name'])
 
-
     #FIXME: not final
-    return render_template("user/recently_played_tracks.html", recently_played_tracks=recently_played_tracks)
+
+    # returns JSON data to be returned to frontend
+    return{'recent_tracks': recently_played_tracks}
+
+    # return render_template("user/recently_played_tracks.html", recently_played_tracks=recently_played_tracks)
+
+    # FOR TESTING
+    # print('This is standard output', file=sys.stdout)
+    # print(jsonify({'recent_tracks': recently_played_tracks}), file=sys.stdout)
