cd123e0746b6bf48313062327311bdfb95a52a82
magacula <makoigacula@gmail.com>
2021-02-02 19:27:40 -0800

    created audio features graph (example)

--- client/src/components/TopTracksGraph.js
+++ client/src/components/TopTracksGraph.js
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import AudioFeatureChart from "./AudioFeaturesChart";
+
+const ChartContainer = styled.div`
+  padding: 2rem;
+`;
 
 const TopTracksGraph = () => {
   return (
     <React.Fragment>
       <h1>Top Tracks Graph</h1>
+      <ChartContainer>
+        <AudioFeatureChart />
+      </ChartContainer>
     </React.Fragment>
   );
 };
