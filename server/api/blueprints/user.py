e19b05ee1f95e76510d2eb947eed2a34b68c3760
lazytosay <siweisijiao.weebly@gmail.com>
2021-02-03 21:42:57 -0800

    updated the return data format in user.py, because you will need their id in order to make them clickable (for other apis)

--- server/api/blueprints/user.py
+++ server/api/blueprints/user.py
@@ -10,8 +10,8 @@ user_bp = Blueprint('user', __name__)
 
 
 @user_bp.route("/user/test")
-# @login_required
-# @token_checked
+@login_required
+@token_checked
 def test():
     #print("---------in test, session: ", session)
     response = make_response(
@@ -48,7 +48,7 @@ def home():
 def top_tracks():
     sp = get_spotify_object()
 
-    top_tracks = []
+    top_tracks = {}
 
     # keep get top tracks until there is no more left
     offset_count = 0
@@ -60,14 +60,19 @@ def top_tracks():
 
         for one_track in top_tracks_raw['items']:
             one_track_name = one_track['name']
-            top_tracks.append(one_track_name)
+            one_track_id = one_track['id']
+            #top_tracks.append(one_track_name)
+            top_tracks[one_track_id] = one_track_name
 
         # if there are less tracks then limit_count, then no need to do another search
         if len(top_tracks_raw['items']) < limit_count:
             break
 
     # FIXME: change this part when need to work with front end
-    return render_template("user/top_tracks.html", top_tracks=top_tracks)
+    #return render_template("user/top_tracks.html", top_tracks=top_tracks)
+
+    #FIXME: current json format: {track_id: track_name}
+    return top_tracks
 
 
 @user_bp.route("/user/top_artists")
@@ -77,7 +82,7 @@ def top_tracks():
 def top_artists():
     sp = get_spotify_object()
 
-    top_artists = []
+    top_artists = {}
 
     # keep get top artists until there is no more left
     offset_count = 0
@@ -89,14 +94,20 @@ def top_artists():
 
         for one_artist in top_artists_raw['items']:
             one_artist_name = one_artist['name']
-            top_artists.append(one_artist_name)
+            one_artist_id = one_artist['id']
+            #top_artists.append(one_artist_name)
+            top_artists[one_artist_id] = one_artist_name
 
         # if there are less tracks then limit_count, then no need to do another search
         if len(top_artists_raw['items']) < limit_count:
             break
 
     # FIXME: change this part when need to work with front end
-    return render_template("user/top_artists.html", top_artists=top_artists)
+    #return render_template("user/top_artists.html", top_artists=top_artists)
+
+
+    #FIXME: current return json: {artist_id: artist_name}
+    return top_artists
 
 
 @user_bp.route("/user/top_albums")
@@ -106,7 +117,7 @@ def top_artists():
 def top_albums():
     sp = get_spotify_object()
 
-    top_albums = []
+    top_albums = {}
 
     # keep get top tracks until there is no more left
     offset_count = 0
@@ -118,15 +129,20 @@ def top_albums():
 
         for one_track in top_tracks_raw['items']:
             cur_album_name = one_track['album']['name']
+            cur_album_id = one_track['album']['id']
             if cur_album_name not in top_albums:
-                top_albums.append(cur_album_name)
+                #top_albums.append(cur_album_name)
+                top_albums[cur_album_id] = cur_album_name
 
         # if there are less tracks then limit_count, then no need to do another search
         if len(top_tracks_raw['items']) < limit_count:
             break
 
     # FIXME: change this part when need to work with front end
-    return render_template("user/top_albums.html", top_albums=top_albums)
+    #return render_template("user/top_albums.html", top_albums=top_albums)
+
+    #FIXME: current json format: {album_id: album_name}
+    return top_albums
 
 
 @user_bp.route("/user/recently_played_tracks")
@@ -136,18 +152,23 @@ def top_albums():
 def recently_played_tracks():
     sp = get_spotify_object()
 
-    recently_played_tracks = []
+    recently_played_tracks = {}
 
     recentlY_played_raw = sp.current_user_recently_played()
 
     for one_record in recentlY_played_raw['items']:
         #time_stamp = one_record["played_at"]
         one_track = one_record['track']
+        one_track_name = one_track['name']
+        one_track_id = one_track['id']
         #recently_played_tracks.append(one_track['name'])
-        recently_played_tracks.append(one_track['name'])
+        recently_played_tracks[one_track_id] = one_track_name
 
     #FIXME: not final
-    return {'recent_tracks': recently_played_tracks}
+    #return {'recent_tracks': recently_played_tracks}
+
+    #FIXME: current json format: {track_id: track_name}
+    return recently_played_tracks
 
     # this is already set in the __init__.py in api folder, but don't delete, for reference later
     # response.headers.add("Access-Control-Allow-Origin",
@@ -159,8 +180,6 @@ def recently_played_tracks():
     # response.set_cookie(key="spotifyTempCookie", value="temp value", samesite='Lax')
     # return response
 
-    # returns JSON data to be returned to frontend
-    # return {'recent_tracks': recently_played_tracks}
 
     # return render_template("user/recently_played_tracks.html", recently_played_tracks=recently_played_tracks)
 
@@ -169,11 +188,11 @@ def recently_played_tracks():
     # print(jsonify({'recent_tracks': recently_played_tracks}), file=sys.stdout)
 
 
-@user_bp.route("/user/user_playlists")
+@user_bp.route("/user/playlists")
 @limiter.limit("2 per second")
 @login_required
 @token_checked
-def user_playlists():
+def playlists():
     sp = get_spotify_object()
     user_playlists = {}
 
