0c1b8a539d1db04472b9f50d92bb4c6c3138d391
magacula <makoigacula@gmail.com>
2021-02-04 14:55:55 -0800

    Component that gets all track info for a single track. Needs to fetch data from track_details

---
+++ client/src/components/Track.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+// TODO:
+// Component that gets all track info for a single track
+// Need to fetch from (/main/track_details/<track_id>)
+
+const Track = () => {
+  return <div></div>;
+};
+
+export default Track;
