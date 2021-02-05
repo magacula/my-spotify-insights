3d949bb94c51a7273d5bf20bb1c02ddbbc800cec
magacula <makoigacula@gmail.com>
2021-02-04 23:51:08 -0800

    created Track component to list basic track info

--- client/src/components/Track.js
+++ client/src/components/Track.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import themes from "../styles/themes";
+const { colors } = themes;
 
 // TODO:
 // Component that gets all track info for a single track
 // Need to fetch from (/main/track_details/<track_id>)
 
-const Track = () => {
-  return <div></div>;
+const TrackContainer = styled(Link)`
+  color: ${colors.white};
+`;
+
+const TrackItem = styled.li`
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const TrackImage = styled.img`
+  width: 20%;
+`;
+
+const TrackInfo = styled.div``;
+
+const TrackName = styled.h4``;
+
+const ArtistName = styled.p``;
+
+const AlbumName = styled.p``;
+
+const Track = ({ track }) => {
+  return (
+    <TrackItem>
+      <TrackContainer to={`/track/${track.id}`}>
+        <TrackImage src={track.album.images[0].url} />
+        <TrackInfo>
+          <div>
+            <TrackName>{track.name}</TrackName>
+            <ArtistName>{track.artists[0].name}</ArtistName>
+            <AlbumName>{track.album.name}</AlbumName>
+          </div>
+        </TrackInfo>
+      </TrackContainer>
+    </TrackItem>
+  );
 };
 
 export default Track;
