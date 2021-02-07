356a3b886e5f7a91e22c2d4ee0927e59368118c7
magacula <makoigacula@gmail.com>
2021-02-06 22:10:00 -0800

    testing data format

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -8,21 +8,21 @@ const UserInfo = () => {
     fetch("user.json")
       .then((res) => res.json())
       .then((data) => {
-        setUserInfo(data);
+        setUserInfo(data.top_tracks);
       });
   }, []);
 
   return (
     <>
-      <div>
-        <p>Username: {userInfo.user_name}</p>
-        <p>Followers: {userInfo.num_followers}</p>
-        <p>Following: {userInfo.user_following_num}</p>
-        <p>Public playlists: {userInfo.num_public_playlists}</p>
-        <p>Private playlists: {userInfo.num_private_playlists}</p>
-        <p>Country: {userInfo.country}</p>
-        <p>Saved Songs: {userInfo.user_saved_songs}</p>
-      </div>
+      <ol style={{ marginLeft: "110px" }}>
+        {userInfo.map((track, index) => {
+          return (
+            <li key={index} style={{ marginLeft: "25px" }}>
+              {track.track.album.name}
+            </li>
+          );
+        })}
+      </ol>
     </>
   );
 };
