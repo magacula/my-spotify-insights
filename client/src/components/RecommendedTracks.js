70c6554982d74c91bcf8303959379bf54fce14be
magacula <makoigacula@gmail.com>
2021-02-09 00:00:54 -0800

    Creates POST request & creates a new playlist (still need to add recommended tracks to playlist)

--- client/src/components/RecommendedTracks.js
+++ client/src/components/RecommendedTracks.js
@@ -2,19 +2,33 @@ import React, { useState, useEffect } from "react";
 import Track from "./Track";
 import styled from "styled-components";
 import themes from "../styles/themes";
+import { NavLink } from "react-router-dom";
 const { colors } = themes;
 
-const ButtonContainer = styled.button`
+const Link = styled(NavLink)`
+  color: ${colors.lightBlue};
+`;
+
+const RecommendationPage = styled(NavLink)``;
+
+const ButtonContainer = styled.div`
+  margin-left: 110px;
+  display: flex;
+  space: 2;
+`;
+
+const Button = styled.button`
   margin-top: 2rem;
   display: inline-block;
   background: ${colors.lightBlue};
   padding: 0.75rem 2.5rem;
   border-radius: 1.5rem;
   border-style: none;
+
+  margin-right: 2rem;
   outline: none;
   color: ${colors.white};
   font-weight: bold;
-  margin-left: 110px;
   cursor: pointer;
 
   &:hover {
@@ -25,6 +39,7 @@ const ButtonContainer = styled.button`
 
 const RecommendedTracks = () => {
   const [recommededTracks, setRecommendedTracks] = useState([]);
+  const [showButton, setShowButton] = useState(false);
 
   const fetchData = async () => {
     try {
@@ -48,14 +63,50 @@ const RecommendedTracks = () => {
 
   const handleClick = () => {
     fetchData();
+    setShowButton(true);
   };
 
   return (
     <React.Fragment>
-      <h2>Recommended Tracks Based on Your Top Tracks:</h2>
-      <ButtonContainer onClick={handleClick}>
-        Get Recommendations
+      <h2>
+        Recommended Tracks Based on <Link to="/Tops">Your Top Tracks:</Link>
+      </h2>
+
+      <ButtonContainer>
+        <Button onClick={handleClick}>Get Recommendations</Button>
+        {showButton && (
+          <Button
+            onClick={async () => {
+              try {
+                const response = await fetch(
+                  `http://127.0.0.1:5000/user/playlists`,
+                  {
+                    method: "POST",
+                    credentials: "include",
+                    //referrerPolicy: 'no-referrer-when-downgrade
+                    mode: "cors",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      name: "Playlist Based on Top Tracks",
+                      public: "True",
+                    }),
+                  }
+                );
+
+                if (response.ok) {
+                  console.log("response worked!");
+                }
+              } catch (error) {
+                console.log(error);
+              }
+            }}>
+            Save Playlist to Spotify
+          </Button>
+        )}
       </ButtonContainer>
+
       <ul style={{ marginLeft: "12rem" }}>
         {recommededTracks.map((track, index) => {
           return (
