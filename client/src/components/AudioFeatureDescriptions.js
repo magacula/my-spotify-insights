8716ec1845ee594a3be5f6e6a1b15b9fa8b3d04d
magacula <makoigacula@gmail.com>
2021-02-09 17:21:52 -0800

    fixed margin on h3

--- client/src/components/AudioFeatureDescriptions.js
+++ client/src/components/AudioFeatureDescriptions.js
@@ -12,13 +12,13 @@ const AudioFeatureDescriptions = () => {
           0.0 to 1.0.
         </p>
         <br></br>
-        <h3>Acousticness</h3>
+        <h3 style={{ margin: 0 }}>Acousticness</h3>
         <p>
           Acousticness measures whether the track is acoustic. A higher score
           reflects a higher confidence that the track is acoustic.
         </p>
         <br></br>
-        <h3>Danceability</h3>
+        <h3 style={{ margin: 0 }}>Danceability</h3>
         <p>
           Danceability measures how suitable a track is for dancing based on a
           combination of musical elements. These elements include tempo, rhythm
@@ -26,7 +26,7 @@ const AudioFeatureDescriptions = () => {
           reflects a song being more danceable.
         </p>
         <br></br>
-        <h3>Energy</h3>
+        <h3 style={{ margin: 0 }}>Energy</h3>
         <p>
           Energy measures the intensity and activity of a song. Some tracks feel
           fast, loud, and noisy while others feel slower and more tones down.
@@ -35,27 +35,27 @@ const AudioFeatureDescriptions = () => {
           more energetic a track is.
         </p>
         <br></br>
-        <h3>Speechiness</h3>
+        <h3 style={{ margin: 0 }}>Speechiness</h3>
         <p>
           Speechiness measures the presence of spoken words in a track. Talk
           shows, audio books, and poetry will reflect a higher speechiness
           score.
         </p>
         <br></br>
-        <h3>Instrumentalness</h3>
+        <h3 style={{ margin: 0 }}>Instrumentalness</h3>
         <p>
           Instrumentalness predicts whether a track contains no vocals. Tracks
           with higher values refect less vocal content such as singing or spoken
           word.
         </p>
         <br></br>
-        <h3>Liveness</h3>
+        <h3 style={{ margin: 0 }}>Liveness</h3>
         <p>
           Liveness detects the presence of an audience in the track. Higher
           values reflect a higher confidence that the track was performed live.
         </p>
         <br></br>
-        <h3>Valence</h3>
+        <h3 style={{ margin: 0 }}>Valence</h3>
         <p>
           Valence describes the musical positiveness conveyed by a track. Tracks
           with high valence can be described as being more cheerful, happy, and
