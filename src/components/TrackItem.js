3ad31332b40d6812f95a2f7593a958c3c0d53fa3
magacula <makoigacula@gmail.com>
2021-02-25 00:29:51 -0800

    Update TrackItem.js

--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -13,12 +13,11 @@ const TrackTitle = styled.div`
   width: 100%;
   display: grid;
   align-items: center;
-  align-content: center;
-  justify-items: center;
+  align-content: right;
 `;
 
 const TrackCover = styled.img`
-  width: 600px;
+  width: 500px;
   margin-left: 8rem;
 `;
 
@@ -29,10 +28,9 @@ const Track = styled.h1`
 `;
 
 const Artist = styled.h2`
-  font-size: 2rem;
-  margin-bottom: 0.6rem;
-  margin-left: 0;
-  margin-top: 0;
+  font-size: 1.5rem;
+  margin-left: 2.5rem;
+  margin-top: 0.5rem;
 `;
 
 const TrackInfo = styled.div`
@@ -46,12 +44,12 @@ const TrackInfo = styled.div`
 
 const Duration = styled.p`
   font-size: 2.5rem;
-  font-weight: 500;
+  font-weight: 600;
 `;
 
 const Popularity = styled.p`
   font-size: 2.5rem;
-  font-weight: 500;
+  font-weight: 600;
 `;
 
 const TrackItem = (props) => {
@@ -72,12 +70,12 @@ const TrackItem = (props) => {
 
   return (
     <React.Fragment>
-      <div style={{ marginLeft: "100px", marginTop: "50px" }}>
+      <div style={{ marginLeft: "100px", marginTop: "100px" }}>
         <TrackContainer>
           <TrackCover src={imageUrl} alt="" />
           <TrackTitle>
-            <Track>{name}</Track>
-            <Artist style={{ marginTop: "0.5rem" }}>{`By: ${artist}`}</Artist>
+            <Track style={{ marginLeft: "2rem" }}>{name}</Track>
+            <Artist>{artist}</Artist>
           </TrackTitle>
         </TrackContainer>
 
