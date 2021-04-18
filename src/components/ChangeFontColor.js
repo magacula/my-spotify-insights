61a58a8c51a531c5273b2d0b5d33f80d391bcda7
akudamoo <57413555+akudamoo@users.noreply.github.com>
2021-04-17 18:09:54 -0700

    Added feature that changes color of all font in application

---
+++ src/components/ChangeFontColor.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import themes from "../styles/themes";
+const { colors } = themes;
+
+//const Background = styled(NavLink)``;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  space: 2;
+`;
+
+const ColorButton = styled.button`
+  margin-top: 2rem;
+  display: inline-block;
+  background: ${colors.lightBlue};
+  padding: 0.75rem 1.5rem;
+  border-radius: 1rem;
+  border-style: none;
+  margin-right: 1rem;
+  outline: none;
+  color: ${colors.white};
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    transition: 0.35s;
+    background: ${colors.pink};
+  }
+`;
+
+const ChangeFontColor = () => {
+    const [backgrounds, setBackgrounds] = useState([]);
+    const [showButton, setShowButton] = useState(false);
+
+    const fetchData = async () => {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    //default
+    const white = () => {
+
+        [...document.getElementsByTagName("*")].forEach(
+            (x) => (x.style["color"] = "white")
+        );
+    };
+
+    const black = () => {
+
+        [...document.getElementsByTagName("*")].forEach(
+            (x) => (x.style["color"] = "black")
+        );
+    };
+
+    const pink = () => {
+        //#ec42f5
+
+        [...document.getElementsByTagName("*")].forEach(
+            (x) => (x.style["color"] = "#ec42f5")
+        );
+    };
+
+    const blue = () => {
+        //#53cced
+
+        [...document.getElementsByTagName("*")].forEach(
+            (x) => (x.style["color"] = "#00b3ff")
+        );
+    };
+
+    const purple = () => {
+        //#a352ff
+
+        [...document.getElementsByTagName("*")].forEach(
+            (x) => (x.style["color"] = "#a352ff")
+        );
+    }
+
+
+    return (
+        <React.Fragment>
+            <ButtonContainer>
+                <ColorButton onClick={white}>White</ColorButton>
+                <ColorButton onClick={black}>Black</ColorButton>
+                <ColorButton onClick={pink}>Pink</ColorButton>
+                <ColorButton onClick={blue}>Blue</ColorButton>
+                <ColorButton onClick={purple}>Purple</ColorButton>
+            </ButtonContainer>
+        </React.Fragment>
+    );
+};
+
+export default ChangeFontColor;
